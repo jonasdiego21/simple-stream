671fd379f007672c80c122ff5c1d94505832c87f
Jonas Diego <jonasdiegorodriguesmartins@yahoo.com.br>
2020-05-30 14:36:56 -0300

    Modify port for testing

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
+const port = process.env.PORT || 3000
 
 const app = express()
+app.use(cors())
 
 app.use(express.static('.'))
 app.get('/', (request, response) => {
     response.send(path.resolve('index.html'))
 })
 
-app.listen(3000)
+app.listen(port, () => console.log(`Escutando na porta ${port}`))
